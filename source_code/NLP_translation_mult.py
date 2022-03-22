8be77ee095a00153b9d5c1f20a379f93d6c0e8b8
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-03-22 23:32:18 +0200

    Update NLP_translation_mult.py

--- source_code/NLP_translation_mult.py
+++ source_code/NLP_translation_mult.py
@@ -3,7 +3,7 @@ from textblob import TextBlob
 
 blob = TextBlob('Ποιο είναι το όνομά σου?') # Works like input() but for a TextBlob object
 
-english = blob.translate(to='en') # To "to" είναι η λέξη κλειδί για την γλώσσα μετάφρασής
+english = blob.translate(to='en') # To "to" είναι η λέξη κλειδί για τη γλώσσα μετάφρασης
 print(english)
 
 spanish = blob.translate(to='es')
